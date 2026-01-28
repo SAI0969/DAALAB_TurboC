a35df31eded75cefbbda26af1b56c77a2ce7e8b4
SAI0969 <asm1scs590526@gmail.com>
2026-01-28 19:05:18 +0530

    Update Max_element.c

--- Week2/Max_element.c
+++ Week2/Max_element.c
@@ -2,32 +2,32 @@
 #include <conio.h>
 
 int main() {
-    int i, n, min, max;
-    int arr[100];
+int i, n, min, max;
+int arr[100];
 
-    clrscr();
+clrscr();
 
-    printf("Enter the number of elements in the array: ");
-    scanf("%d", &n);
+printf("Enter the number of elements in the array: ");
+scanf("%d", &n);
 
-    for(i = 0; i < n; i++) {
-        printf("Enter element %d: ", i + 1);
-        scanf("%d", &arr[i]);
-    }
+for(i = 0; i < n; i++) {
+printf("Enter element %d: ", i + 1);
+scanf("%d", &arr[i]);
+}
 
-    min = max = arr[0];
+min = max = arr[0];
 
-    for(i = 1; i < n; i++) {
-        if(arr[i] > max)
-            max = arr[i];
+for(i = 1; i < n; i++) {
+if(arr[i] > max)
+max = arr[i];
 
-        if(arr[i] < min)
-            min = arr[i];
-    }
+if(arr[i] < min)
+min = arr[i];
+}
 
-    printf("Maximum element = %d\n", max);
-    printf("Minimum element = %d", min);
+printf("Maximum element = %d\n", max);
+printf("Minimum element = %d", min);
 
-    getch();
-    return 0;
+getch();
+return 0;
 }
