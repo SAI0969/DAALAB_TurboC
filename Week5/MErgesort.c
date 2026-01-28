940f64e0d2e90c8520dc0f4f478b34bf85126228
SAI0969 <asm1scs590526@gmail.com>
2026-01-29 04:18:01 +0530

    Update MErgesort.c

--- Week5/MErgesort.c
+++ Week5/MErgesort.c
@@ -1,56 +1,72 @@
 #include<stdio.h>
 #include<conio.h>
 
-void merge(int a[],int low,int mid,int high){
-int i=low,j=mid+1,k=low;
-int temp[100];
-while(i<=mid&&j<=high){
-if(a[i]<a[j]){
-temp[k]=a[i];
-i++;
+void merge(int b[],int p,int c[],int q,int a[]){
+int i=0;
+int j=0;
+int k=0;
+while(i<p&&j<q){
+if(b[i]<c[j]){
+a[k]=b[i];
+i=i+1;
+k=k+1;
 }
 else{
-temp[k]=a[j];
-j++;
+a[k]=c[j];
+j=j+1;
+k=k+1;
 }
-k++;
 }
-while(i<=mid){
-temp[k]=a[i];
-i++;
-k++;
+if(i==p){
+while(j<q){
+a[k]=c[j];
+j=j+1;
+k=k+1;
 }
-while(j<=high){
-temp[k]=a[j];
-j++;
-k++;
 }
-for(i=low;i<=high;i++)
-a[i]=temp[i];
+else{
+while(i<p){
+a[k]=b[i];
+i=i+1;
+k=k+1;
+}
+}
 }
 
-void merge_sort(int a[],int low,int high){
+void merge_sort(int a[],int n){
 int mid;
-if(low<high){
-mid=(low+high)/2;
-merge_sort(a,low,mid);
-merge_sort(a,mid+1,high);
-merge(a,low,mid,high);
+int b[50];
+int c[50];
+int i,j;
+if(n>1){
+mid=n/2;
+for(i=0;i<mid;i=i+1)
+b[i]=a[i];
+j=0;
+for(i=mid;i<n;i=i+1){
+c[j]=a[i];
+j=j+1;
+}
+merge_sort(b,mid);
+merge_sort(c,n-mid);
+merge(b,mid,c,n-mid,a);
 }
 }
 
 int main(){
-int a[100],n,i;
+int n;
+int a[100];
+int i;
 clrscr();
 printf("Enter number of elements:");
 scanf("%d",&n);
 printf("Enter elements:");
-for(i=0;i<n;i++)
+for(i=0;i<n;i=i+1)
 scanf("%d",&a[i]);
-merge_sort(a,0,n-1);
+merge_sort(a,n);
 printf("Sorted array:");
-for(i=0;i<n;i++)
-printf("%d ",a[i]);
+for(i=0;i<n;i=i+1)
+printf("%d",a[i]);
 getch();
 return 0;
 }
