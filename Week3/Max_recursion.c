a862d7b6ddff98852529825b1dd67118a2c7667b
SAI0969 <asm1scs590526@gmail.com>
2026-01-28 19:05:49 +0530

    Fix formatting and add Find_Min function

--- Week3/Max_recursion.c
+++ Week3/Max_recursion.c
@@ -1,33 +1,43 @@
-#include <stdio.h>
-#include <conio.h>
+#include<stdio.h>
+#include<conio.h>
 
-int Find_Max(int arr[], int n){
-if(n == 1)
+int Find_Max(int arr[],int n){
+if(n==1)
 return arr[0];
-
-if(arr[n - 1] > Find_Max(arr, n - 1))
-return arr[n - 1];
+if(arr[n-1]>Find_Max(arr,n-1))
+return arr[n-1];
 else
-return Find_Max(arr, n - 1);
+return Find_Max(arr,n-1);
+}
+
+int Find_Min(int arr[],int n){
+if(n==1)
+return arr[0];
+if(arr[n-1]<Find_Min(arr,n-1))
+return arr[n-1];
+else
+return Find_Min(arr,n-1);
 }
 
 int main(){
-int n, i, max;
+int n,i,max,min;
 int arr[100];
 
 clrscr();
 
-printf("Enter the number of elements : ");
-scanf("%d", &n);
+printf("Enter number of elements:");
+scanf("%d",&n);
 
-for(i = 0; i < n; i++){
-printf("Enter element %d : ", i);
-scanf("%d", &arr[i]);
+for(i=0;i<n;i++){
+printf("Enter element%d:",i+1);
+scanf("%d",&arr[i]);
 }
 
-max = Find_Max(arr, n);
+max=Find_Max(arr,n);
+min=Find_Min(arr,n);
 
-printf("The max element is %d", max);
+printf("Max=%d\n",max);
+printf("Min=%d",min);
 
 getch();
 return 0;
